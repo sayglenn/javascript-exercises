6e66bd8f33aa9892dec719047409b171dd9d0369
Say Glenn <glennsay1234@gmail.com>
2024-02-10 23:56:49 +0800

    Completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR"
+    } else if (times == 0) {
+        return "";
+    } else {
+        return word + repeatString(word, times - 1);
+    }
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else {
+        if (num1 > num2) {
+            let result = 0;
+            for (let i = num2; i < num1 + 1; i++) {
+                result += i;
+            }
+            return result;
+        } else {
+            let result = 0;
+            for (let i = num1; i < num2 + 1; i++) {
+                result += i;
+            }
+            return result;
+        }
+    }
 };
 
 // Do not edit below this line
